d5f5e1c9ad21bb5973883e3cf7410c36c388def1
Darren Cook <cookd.liv@gmail.com>
2021-06-29 18:55:50 +0100

    added RF to main-pipeline.py

--- python/accommodation_features.py
+++ python/accommodation_features.py
@@ -74,7 +74,18 @@ def SubCondProb(dataframe):
         
         # parse individual tuple elements
         chat_id = chat[0]
-        chatlog = chat[1]
+        chatlog = chat[1].reset_index()
+        
+        
+        # record chat type (1=pred, 0=np)
+        if sum(chatlog['speaker_type'])>0:
+            chat_type = 1
+        else:
+            chat_type = 0
+
+        
+        # fill nans
+        chatlog['content'] = chatlog['content'].fillna('')
         
         # clean msg
         chatlog['content'] = chatlog['content'].apply(lambda x: msg_clean(x))
@@ -82,8 +93,10 @@ def SubCondProb(dataframe):
         
         
         # merge adjacent turns that share the same speaker
-        chatlog['prev_speaker'] = chatlog['speaker'].shift(1).mask(pd.isnull, chatlog['speaker'])
-        chatlog['speaker_change'] = np.where(chatlog['speaker'] != chatlog['prev_speaker'], 'True','False')
+        chatlog['prev_speaker'] = chatlog[
+            'speaker'].shift(1).mask(pd.isnull, chatlog['speaker'])
+        chatlog['speaker_change'] = np.where(
+            chatlog['speaker'] != chatlog['prev_speaker'], 'True','False')
         
         # create list to store speaker change keys
         speaker_changes = []    
@@ -103,46 +116,116 @@ def SubCondProb(dataframe):
         chatlog['chunking'] = speaker_changes
         
         # join adjacent messages sharing the same speaker ID 
-        chatlog['content'] = chatlog.groupby(['chunking'])['content'].transform(lambda x: ' '.join(x))
+        chatlog['content'] = chatlog.groupby(
+            ['chunking'])['content'].transform(lambda x: ' '.join(x))
         
         # drop duplicate rows based on message content and conversation chunk
         chatlog = chatlog.drop_duplicates(subset=['content','chunking'])
         
         # drop non-needed columns
-        chatlog = chatlog[['speaker','content', 'speaker_type','time','chat_id']]
+        chatlog = chatlog[['speaker','content', 'speaker_type',
+                           'time','chat_id']]
         
         
         
-        # parse msg into n-grams
-        for ngram_size in range(2,3):
-            chatlog['content'] = chatlog['content'].apply(
+        # if non-predatory chat, create pseudo-speaker roles
+        if chat_type==0:
+            
+            speaker_ids = list(set(chatlog['speaker']))
+            
+            chatlog['speaker_type'] = np.where(chatlog['speaker']==speaker_ids[0], 1, 0)
+        
+        
+        # store chat level scp scores 
+        scp_vals_local = {}
+        
+        # number of n-grams
+        for ngram_size in range(1,4):
+
+            ## pre-process msg content for SCP analysis
+            
+            # duplicate df
+            chatlog_scp = chatlog.copy(deep=True)
+            
+            # parse msg into n-grams
+            chatlog_scp['content'] = chatlog_scp['content'].apply(
                 lambda x: ngramize(x, ngram_size))
             
             # take set of tokens in msg
-            chatlog['content'] = chatlog['content'].apply(
+            chatlog_scp['content'] = chatlog_scp['content'].apply(
                 lambda x: list(set(x)))
             
                       
             # compare each turn with the previous
-            chatlog['previous'] = chatlog['content'].shift().fillna('')
+            chatlog_scp['previous'] = chatlog_scp['content'].shift().fillna('')
             
             # get intersection between each turn and prior
-            chatlog['shared'] = chatlog.apply(
+            chatlog_scp['shared'] = chatlog_scp.apply(
                 lambda x: np.intersect1d(x.content, x.previous), axis=1)
             
             
-            # # get the token set of IR speech
-            # ir = chatlog[chatlog['speaker'] ==1]
-            # # collate all n-grams into one list
-            # ir_turn_set = [token for turn in ir['content'] for token in turn]
-            # # create counter object of each n-gram
-            # subtrahend = Counter(token for token in ir_turn_set)
-            # # divide each value by the number of IR turns
-            # for token, counter in subtrahend.items():
-            #     subtrahend[token] /= len(ir)
+            ## (eq.1) s1 probability per ngram
             
+            # filter on speaker 1 msgs
+            s1_msgs = chatlog_scp[chatlog_scp['speaker_type'] ==1]
+                                               
+            # collate all n-grams spoken by speaker 1 into one list
+            s1_tokens = [token for turn in s1_msgs['content'] for token in turn]
+            
+            # create counter object of each n-gram
+            from collections import Counter
+            s1_ngram_count = Counter(token for token in s1_tokens)
+            
+            # divide each value by the number of IR turns
+            for token, counter in s1_ngram_count.items():
+                s1_ngram_count[token] /= len(s1_msgs)
 
             
-        
+            ## (eq.2) conditional probability per ngram given prior usage by s0
+            
+            # collate shared n-grams into one list
+            s1_shared_set = [token for turn in s1_msgs['shared'] for token in turn]
+            
+            # create counter object of each n-gram
+            shared_counter = Counter(token for token in s1_shared_set)
+            
+            # get list of s0 tokens
+            s0_tokens = [token for turn in s1_msgs['previous'] for token in turn]
+            # create counter
+            s0_ngram_count = Counter(token for token in s0_tokens)
+            
+            # get final value for minuend
+            ngram_conditional = {k : v / s0_ngram_count[k] for k, v in shared_counter.items() if k in s0_ngram_count}  
+            
+            # identify non-converged S1 tokens
+            tokenDiff = list(set(s1_tokens).difference(s1_shared_set))
+            
+            # create dictionary where the conditional probability is zero
+            divergedTokens = dict.fromkeys(tokenDiff, float(0))
+            ngram_conditional.update(divergedTokens)
+            
+            
+            ## subtractive conditional probability scores (SCP = eq.2 - eq.1)
+            
+            # subtract eq1 from eq2 to get final scp score per ngram
+            scp_scores = {k : v - s1_ngram_count[k] for k, v in ngram_conditional.items() if k in s1_ngram_count}
+            
+            # add scp scores to local dict
+            scp_vals_local.update(scp_scores)
+            
+            
+        # add (chat level) local SCP scores to main (global) dict
+        scp_vals_global.update({chat_id: scp_vals_local})
+            
+     
+    ## feature selection    
     
-    return chatlog
+    # format scp features as a matrix
+    scp_features = pd.DataFrame.from_dict(scp_vals_global, orient='index').fillna(0)
+            
+    # remove features appearing in less than 10% of corpus
+    min_nonZero = int(len(scp_features)*0.1)
+    scp_features = scp_features.loc[:, (scp_features.replace(0, np.nan).notnull().sum(axis=0) >= min_nonZero)]
+    
+
+    return scp_features
--- python/main-pipeline.py
+++ python/main-pipeline.py
@@ -5,6 +5,7 @@ Main pipeline for analysing chatlogs scraped from PAN12 xml file.
 
 import os
 
+import numpy as np
 import pandas as pd
 
 
@@ -47,6 +48,99 @@ data = preprocess_xml(xml_data, predators, resampled)
 
 #### Feature Engineering
 
-# Subtractive Conditional Probability - Danescu-Nicelescu et al, 2012
+# Subtractive Conditional Probability - Danescu-Nicelescu et al, 2012 
 from accommodation_features import SubCondProb
 bow_1 = SubCondProb(data)
+
+
+# Add other features here......
+
+
+
+
+
+#### Chat Type Classification
+
+# add labels to feature vector
+chat_type = data.groupby(['chat_id']).max(['speaker_type'])
+
+chat_type['speaker_type'].value_counts()
+
+
+# merge chat type labels with feature vector
+# group other features here......
+features = pd.merge(bow_1, chat_type, left_index=True, right_index=True)
+
+# set X and y vars
+y = np.array(features['speaker_type'])
+X = np.array(features.drop(['speaker_type','msg_id'], axis=1).reset_index(drop=True))
+
+# prep cls task
+from sklearn.ensemble import RandomForestClassifier
+
+clsfr = RandomForestClassifier(n_estimators=100,
+                               max_depth=None,
+                               min_samples_split=2,
+                               min_samples_leaf=1,
+                               min_weight_fraction_leaf=0.0,
+                               max_features='auto',
+                               max_leaf_nodes=None,
+                               min_impurity_decrease=0.0,
+                               min_impurity_split=None,
+                               bootstrap=True,
+                               oob_score=False,
+                               n_jobs=-1,
+                               random_state=123,
+                               warm_start=False,
+                               class_weight=None,
+                               ccp_alpha=0.0,
+                               max_samples=None)
+
+
+# prep Kfold
+from sklearn.model_selection import StratifiedKFold
+
+cross_val = StratifiedKFold(n_splits=10,
+                            shuffle=False,
+                            random_state=None)
+
+
+# store preds over all folds
+all_preds = []
+
+# store performance metrics per fold
+from sklearn.metrics import confusion_matrix, classification_report
+conf_metrices = []
+
+reports = []
+
+# run model
+for train_index, test_index in cross_val.split(X, y):
+    
+    # fold counter
+    fold_num = 1
+    
+    # set train and test regions
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    
+    # fit model
+    clsfr.fit(X_train, y_train)
+    
+    # get predictions
+    y_preds = clsfr.predict(X_test)
+    
+    all_preds.append([y_test, y_preds])
+    
+    # performance
+    output = confusion_matrix(y_test, y_preds)
+    conf_metrices.append(output)
+    
+    # classification report
+    target_names = ['Non-Pred','Pred']
+    report = classification_report(y_test, y_preds, target_names=target_names)
+    reports.append(report)
+    
+    
+    
+    fold_num +=1
